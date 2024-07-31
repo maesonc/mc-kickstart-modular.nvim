1d69cc7366e5d2db0283aef946d5962b61d7e13e
Maeson Chan <maeson@cokeeps.com>
2024-07-31 21:11:33 +0800

    test

--- lua/custom/plugins/main/statuscol.lua
+++ lua/custom/plugins/main/statuscol.lua
@@ -17,7 +17,7 @@ return {
           { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
         },
       }
-      local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+      local signs = { Error = ' x', Warn = ' ', Hint = ' ', Info = ' ' }
       for type, icon in pairs(signs) do
         local hl = 'DiagnosticSign' .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
